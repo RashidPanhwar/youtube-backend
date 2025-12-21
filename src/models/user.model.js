c31e7496b768c66599b37cdff0a826d6aac41d11
rashidpanhwer8 <rashidpanhwer8@gmail.com>
2025-12-21 05:07:29 +0500

    cloudinary and multer integrated

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -59,4 +59,30 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            full_name: this.full_name,
+        },
+        process.env.ACCESS_TOKEN_SECRATE,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRATE,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
